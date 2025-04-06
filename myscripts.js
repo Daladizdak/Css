c29bce29bcb227e3d38f365739f9847bdb24aee4
Daladizdak <a.h.shekarinamini@wlv.ac.uk>
2025-04-06 14:39:34 +0100

    Add files via upload

--- myscripts.js
+++ myscripts.js
@@ -2,51 +2,61 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
 
 
-
 // Add SDKs for Firebase products that you want to use
-import { Firestore, 
-		 getFirestore, 
-		 onSnapshot, 
-		 query, 
-		 collection, 
-		 orderBy,
-		 addDoc } from 'https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js'
+import { Firestore,
+getFirestore,
+onSnapshot,
+query,
+collection,
+orderBy,
+addDoc } from 'https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js'
+
 
 
 // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyDzDHpsWpGbIxXc6boCFiXJa6ZTAiRIlcI",
-    authDomain: "amir5cs022.firebaseapp.com",
-    projectId: "amir5cs022",
-    storageBucket: "amir5cs022.firebasestorage.app",
-    messagingSenderId: "762490470175",
-    appId: "1:762490470175:web:b793b64be078c531be3a2a"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyA065c8czE8dwdjZmer-pfN104xMAkfrr8",
+  authDomain: "assignment-4a14e.firebaseapp.com",
+  projectId: "assignment-4a14e",
+  storageBucket: "assignment-4a14e.firebasestorage.app",
+  messagingSenderId: "346473966556",
+  appId: "1:346473966556:web:33d5ade95f8e1325ab7d19"
+};
+
 
 
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);	
+const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 
+
+
+
 // Get a live data snapshot (i.e. auto-refresh) of our Reviews collection
-const q = query(collection(db, "Reviews"), orderBy("book_name"));
+const q = query(collection(db, "Movies"), orderBy("movie_name"));
 const unsubscribe = onSnapshot(q, (snapshot) => {
 
-  // Empty HTML table
-  $('#reviewList').empty();
-	
-  // Loop through snapshot data and add to HTML table
-  var tableRows = '';
-  snapshot.forEach((doc) => {
-	tableRows += '<tr>';
-	tableRows += '<td>'  + doc.data().book_name + '</td>';
-	tableRows += '<td>'  + doc.data().book_rating + '/5</td>';
-	tableRows += '</tr>';	  
-  });
-  $('#reviewList').append(tableRows);
-	
-  // Display review count
-  $('#mainTitle').html(snapshot.size + " book reviews in the list");
+
+// Empty HTML table
+$('#reviewList').empty();
+
+
+
+// Loop through snapshot data and add to HTML table
+var tableRows = '';
+snapshot.forEach((doc) => {
+tableRows += '<tr>';
+tableRows += '<td>' + doc.data().move_name + '</td>';
+tableRows += '<td>' + doc.data().move_director + '</td>';
+tableRows += '<td>' + doc.data().move_release + '</td>';
+tableRows += '<td>' + doc.data().movie_rating + '/5</td>';
+tableRows += '</tr>';
+});
+$('#reviewList').append(tableRows);
+
+
+// Display review count
+$('#mainTitle').html(snapshot.size + " Movie reviews in the list");
 });
